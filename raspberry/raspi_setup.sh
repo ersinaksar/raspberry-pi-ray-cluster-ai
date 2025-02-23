1077927455f1956d6fea3dea41c69e32d1ec66e3
Ersin Akşar <ersinaksar@yandex.com>
2025-02-23 10:12:44 +0300

    Update raspi_setup.sh

--- raspberry/raspi_setup.sh
+++ raspberry/raspi_setup.sh
@@ -1,26 +1,26 @@
 
 #!/bin/bash
 
-# Gerekli bağımlılıkları yükleyin
+
 sudo apt update
 sudo apt install -y wget build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev
 
-# Python 3.10 kaynak kodunu indirin ve çıkartın
+# download extract and compile Python 3.10 source code
 wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz
 tar -xf Python-3.10.4.tgz
 cd Python-3.10.4
 
-# Python 3.10'u derleyin ve kurun
+# setup Python 3.10
 ./configure --enable-optimizations
-make -j 4  # 4, Raspberry Pi'nin işlemci çekirdek sayısına göre ayarlanabilir
+make -j 4  # 4, can be set depends on Raspberry Pi's core processor 
 sudo make altinstall
 
-# Sanal ortamı oluşturun ve aktifleştirin
+# create and activat ve
 cd ..
 python3.10 -m venv myenv
 source myenv/bin/activate
 
-# Sanal ortamın Python sürümünü kontrol edin
+# check the version
 python --version
 
 
